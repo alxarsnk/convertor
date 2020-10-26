49b3da89d4562ae629f4dc7751af83ccbdc618ad
Alexandr <aleksandr@startted.com>
2020-10-26 15:46:39 +0300

    initial commit

---
+++ Convertor/Convertor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Convertor
+//
+//  Created by Alexandr Arsenyuk on 26.10.2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Convertor/Convertor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Convertor
+//
+//  Created by Alexandr Arsenyuk on 26.10.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
