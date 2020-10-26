3f3e15794bbc10f2d09da66c1f110bcac6b6d238
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2020-10-26 23:48:34 +0300

    Парсинг storyboard в консоль

---
+++ Convertor/Convertor/CustomUI/DropView.swift
@@ -0,0 +1,69 @@
+//
+//  DropView.swift
+//  Convertor
+//
+//  Created by Александр Арсенюк on 26.10.2020.
+//
+
+import Cocoa
+
+protocol DropViewDelegate: AnyObject {
+    func performDragOperation(_ sender: NSDraggingInfo) -> Bool
+}
+
+class DropView: NSView {
+
+    weak var delegate: DropViewDelegate?
+    
+    var filePath: String?
+    let expectedExt = ["storyboard"]
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        self.wantsLayer = true
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+
+        registerForDraggedTypes([NSPasteboard.PasteboardType.URL, NSPasteboard.PasteboardType.fileURL])
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        // Drawing code here.
+    }
+
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        if checkExtension(sender) == true {
+            self.layer?.backgroundColor = NSColor.blue.cgColor
+            return .copy
+        } else {
+            return NSDragOperation()
+        }
+    }
+
+    fileprivate func checkExtension(_ drag: NSDraggingInfo) -> Bool {
+        guard let board = drag.draggingPasteboard.propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? NSArray,
+              let path = board[0] as? String
+        else { return false }
+
+        let suffix = URL(fileURLWithPath: path).pathExtension
+        for ext in self.expectedExt {
+            if ext.lowercased() == suffix {
+                return true
+            }
+        }
+        return false
+    }
+
+    override func draggingExited(_ sender: NSDraggingInfo?) {
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+
+    override func draggingEnded(_ sender: NSDraggingInfo) {
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        return delegate?.performDragOperation(sender) ?? true
+    }
+}
--- Convertor/Convertor/ViewController.swift
+++ Convertor/Convertor/ViewController.swift
@@ -8,11 +8,17 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var leftDropView: DropView!
+    @IBOutlet weak var leftLabel: NSTextField!
+    
+    @IBOutlet weak var rightDropView: NSView!
+    @IBOutlet weak var rightLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLabels()
+        setupDropViews()
     }
 
     override var representedObject: Any? {
@@ -20,7 +26,37 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    private func setupLabels() {
+        leftLabel.alignment = .center
+        rightLabel.alignment = .center
+    }
+    
+    private func setupDropViews() {
+        rightDropView.wantsLayer = true
+        rightDropView.layer?.backgroundColor = NSColor.gray.cgColor
+        leftDropView.delegate = self
+    }
 
+}
 
+extension ViewController: DropViewDelegate {
+    
+    func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        guard let pasteboard = sender.draggingPasteboard.propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? NSArray,
+              let path = pasteboard[0] as? String
+        else { return false }
+        print("FilePath: \(path)")
+        if let aStreamReader = StreamReader(path: path) {
+            defer {
+                aStreamReader.close()
+            }
+            while let line = aStreamReader.nextLine() {
+                print(line)
+            }
+        }
+        return true
+    }
+    
 }
 
