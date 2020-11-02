7f6cb613e6eadaa25741ba6ebc4346c0723e5914
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2020-11-02 10:01:29 +0300

    MVP version

--- Convertor/Convertor/ViewController.swift
+++ Convertor/Convertor/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var rightDropView: NSView!
     @IBOutlet weak var rightLabel: NSTextField!
+   
+    @IBOutlet weak var saveButton: NSButton!
+    
+    private var fileText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLabels()
         setupDropViews()
+        saveButton.title = "Save"
+        saveButton.contentTintColor = .white
+        saveButton.layer?.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        saveButton.isHidden = true
     }
 
     override var representedObject: Any? {
@@ -37,7 +45,39 @@ class ViewController: NSViewController {
         rightDropView.layer?.backgroundColor = NSColor.gray.cgColor
         leftDropView.delegate = self
     }
+    
+    private func saveButtonPressed() {
+        let savePanel = NSSavePanel()
+        savePanel.canCreateDirectories = true
+        savePanel.showsTagField = true
+        savePanel.allowedFileTypes = ["swift"]
+        savePanel.allowsOtherFileTypes = false
+        savePanel.nameFieldStringValue = "generated"
+        savePanel.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(.modalPanelWindow)))
+        savePanel.begin { (result) in
+            if result.rawValue == NSApplication.ModalResponse.OK.rawValue {
+                let filename = savePanel.url
+                do {
+                    try self.fileText.write(to: filename!, atomically: true, encoding: String.Encoding.utf8)
+                } catch let error {
+                    print("error: \(error)")
+                }
+            }
+        }
+    }
+    
+    private func setupCOnvertedReadyState() {
+        rightLabel.isHidden = true
+        saveButton.isHidden = false
+        let imageView = NSImageView(frame: leftDropView.frame)
+        imageView.image = #imageLiteral(resourceName: "swiftFIleIcon")
+        imageView.imageAlignment = .alignCenter
+        rightDropView.addSubview(imageView)
+    }
 
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        saveButtonPressed()
+    }
 }
 
 extension ViewController: DropViewDelegate {
@@ -46,17 +86,23 @@ extension ViewController: DropViewDelegate {
         guard let pasteboard = sender.draggingPasteboard.propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? NSArray,
               let path = pasteboard[0] as? String
         else { return false }
-        print("FilePath: \(path)")
+        if let url = URL(string: path) {
+            leftLabel.stringValue = url.lastPathComponent
+            let imageView = NSImageView(frame: leftDropView.frame)
+            imageView.image = #imageLiteral(resourceName: "storyboardFileIcon")
+            imageView.imageAlignment = .alignCenter
+            leftDropView.addSubview(imageView)
+        }
         if let aStreamReader = StreamReader(path: path) {
             defer {
                 aStreamReader.close()
             }
             while let line = aStreamReader.nextLine() {
-                print(line)
+                fileText.append(line + "\n")
             }
+            setupCOnvertedReadyState()
         }
         return true
     }
-    
 }
 
