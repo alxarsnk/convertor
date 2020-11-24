117cc761778651827c9253679178b8a086693210
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2020-11-24 13:24:47 +0300

    Generate empty swiftUI file

--- Convertor/Convertor/ViewController.swift
+++ Convertor/Convertor/ViewController.swift
@@ -14,10 +14,11 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var rightDropView: NSView!
     @IBOutlet weak var rightLabel: NSTextField!
-   
+    
     @IBOutlet weak var saveButton: NSButton!
     
     private var fileText = ""
+    private var sourceCode = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +29,10 @@ class ViewController: NSViewController {
         saveButton.layer?.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
         saveButton.isHidden = true
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
     
@@ -74,7 +75,7 @@ class ViewController: NSViewController {
         imageView.imageAlignment = .alignCenter
         rightDropView.addSubview(imageView)
     }
-
+    
     @IBAction func saveButtonPressed(_ sender: Any) {
         saveButtonPressed()
     }
@@ -84,25 +85,76 @@ extension ViewController: DropViewDelegate {
     
     func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
         guard let pasteboard = sender.draggingPasteboard.propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? NSArray,
-              let path = pasteboard[0] as? String
+              let path = pasteboard[0] as? String,
+              let url = URL(string: path)
         else { return false }
-        if let url = URL(string: path) {
-            leftLabel.stringValue = url.lastPathComponent
-            let imageView = NSImageView(frame: leftDropView.frame)
-            imageView.image = #imageLiteral(resourceName: "storyboardFileIcon")
-            imageView.imageAlignment = .alignCenter
-            leftDropView.addSubview(imageView)
-        }
+        
+        leftLabel.stringValue = url.lastPathComponent
+        let imageView = NSImageView(frame: leftDropView.frame)
+        imageView.image = #imageLiteral(resourceName: "storyboardFileIcon")
+        imageView.imageAlignment = .alignCenter
+        leftDropView.addSubview(imageView)
         if let aStreamReader = StreamReader(path: path) {
             defer {
                 aStreamReader.close()
             }
             while let line = aStreamReader.nextLine() {
-                fileText.append(line + "\n")
+                sourceCode.append(line + "\n")
             }
             setupCOnvertedReadyState()
         }
+        fileText.append(
+            generateHeader(
+                fileName: String(url.lastPathComponent.dropLast(11))
+            )
+        )
+        
+        fileText.append(
+            generateTemplate(
+                fileName: String(url.lastPathComponent.dropLast(11))
+            )
+        )
         return true
     }
+}
+
+extension ViewController {
+    
+    private func generateHeader(fileName: String) -> String {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return """
+            //
+            //  \(fileName).swift
+            //
+            //  Created by Convertor on \(formatter.string(from: date)).
+            //  Copyright © 2020 Personal Team. All rights reserved.
+            //\n
+            """
+    }
+    
+    private func generateTemplate(fileName: String) -> String {
+        return """
+            import SwiftUI
+
+            struct \(fileName): View {
+                var body: some View {
+                    \(beginParsing())
+                }
+            }
+
+            struct \(fileName)_Previews: PreviewProvider {
+                static var previews: some View {
+                    \(fileName)()
+                }
+            }
+            """
+    }
+    
+    private func beginParsing() -> String {
+        return "Text(\"Parsing\")"
+    }
+    
 }
 
